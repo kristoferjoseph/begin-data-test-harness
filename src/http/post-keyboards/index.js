2f7c117468749bcd338c33895189fc31c371cd8e
kj <kristoferjoseph@gmail.com>
2020-05-01 12:54:29 -0700

    Tests adding Objects, Arrays and Functions

--- src/http/post-keyboards/index.js
+++ src/http/post-keyboards/index.js
@@ -3,6 +3,14 @@ const arc = require('@architect/functions')
 
 exports.handler = async function http (req) {
   let body = arc.http.helpers.bodyParser(req)
+  body.Object = {
+    name: 'MITO LASER',
+    type: 'keycaps'
+  }
+  body.Array = [1, 2, 3]
+  body.Function = function yolo() {
+    return 'YOLO!'
+  }
   let saved = await data.set({
     table: 'keyboards',
     ...body
