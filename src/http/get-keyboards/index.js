7bc946291c865f809d0f963d20e2f936d70174d9
kj <kristoferjoseph@gmail.com>
2020-09-30 15:52:11 -0700

    Adds pagination.

--- src/http/get-keyboards/index.js
+++ src/http/get-keyboards/index.js
@@ -1,8 +1,15 @@
 const data = require('@begin/data')
 
 exports.handler = async function http (req) {
-  let keyboards = await data.get({
-    table: 'keyboards'
+  let pages = await data.get({
+    table: 'keyboards',
+    limit: 25
   })
+
+  let keyboards = []
+  for await (let record of pages) {
+    keyboards.push(record)
+  }
+
   return JSON.stringify(keyboards, null, 2)
 }
